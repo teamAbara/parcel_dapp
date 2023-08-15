899bb2ce7450aea2e70fb063f0693f3265c4d0ce
kyunghyunHan <ekfqlwcjswl@naver.com>
2023-08-15 23:17:33 +0900

    feat:design

---
+++ app/ParcelDetail/[id]/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { Demo } from "../test";
+export default function Courierinquiry() {
+  return (
+    <>
+      <div style={{ marginTop: 150, backgroundColor: "white" }}>
+        <Demo />{" "}
+      </div>
+    </>
+  );
+}
