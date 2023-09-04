9f1ba385a3eda33184abd28d6bc3e7c5577c7a7d
kyunghyunHan <ekfqlwcjswl@naver.com>
2023-09-04 11:23:41 +0900

    feat:loding

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,19 @@
 import { useEffect, useState } from "react";
 import { MainBenner } from "@/components/main/main_benner";
-import { Loader } from "@mantine/core";
+import { LoadingOverlay } from "@mantine/core";
 
 //메인페이지
 const Home = () => {
   /*next js에서 page원할하게 가져올려면 데이머 먼저 로딩후에 페이지 렌더링 */
-  const [mounted, setMounted] = useState(false);
+  const [mounted, setMounted] = useState(true);
   useEffect(() => {
-    setMounted(true);
+    setMounted(false);
   }, []);
   //loding추가
-  return mounted == false ? (
-    <div style={{ marginTop: 150, paddingBottom: 150, textAlign: "center" }}>
-      <Loader size="xl" />
-    </div>
+  return mounted == true ? (
+    <>
+      <LoadingOverlay visible={true} overlayBlur={2} />
+    </>
   ) : (
     <>
       <div style={{ marginTop: 150, paddingBottom: 150 }}>
